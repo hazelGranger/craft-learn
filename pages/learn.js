888a29e4bf054cf893eb742387139e5cb0c9e326
hazelGranger <wangzhen_blcu@163.com>
2021-11-07 15:39:30 +1300

    remove unused components

--- pages/learn.js
+++ pages/learn.js
@@ -1,29 +1,16 @@
 import { useNode } from '@craftjs/core';
 import React from 'react';
 import { Editor, Frame, Element, Canvas, Selector } from '@craftjs/core';
-import { Container } from '../components/user/Container';
 import { Toolbox } from '../components/Toolbox';
 import { SettingsPanel } from '../components/SettingsPanel';
 import { Text } from '../components/user/Text';
-
-const TextComponent = ({ text }) => {
-  const {
-    connectors: { drag },
-  } = useNode();
-
-  return (
-    <div ref={drag}>
-      <h2>{text}</h2>
-    </div>
-  );
-};
 
 export default function App() {
   return (
     <div>
       <div className="editor-container">
         <Editor>
-          <Frame resolver={(TextComponent, Text)}>
+          <Frame resolver={(Text)}>
             <Element is="div" id="my-ele" className="container" canvas>
               <Text
                 size="small"
